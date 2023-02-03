fbfd733ed3406996c9f78bed229ef2e4010816ff
JonKelly <109151932+JonKelly92@users.noreply.github.com>
2023-02-03 09:50:26 -0400

    Added a 2 decimal places when displaying numbers under 1000

--- Assets/Scripts/FormatCurrency.cs
+++ Assets/Scripts/FormatCurrency.cs
@@ -5,18 +5,25 @@ public static class FormatCurrency
 {
     public static string ToCurrency(this double amount)
     {
-        amount = Math.Truncate(amount);
-        int length = amount.ToString().Length;
+        int length = amount.ToString("N2").Length;
 
-        if (length > 12)
+        if (length > 15)
             return amount.ToString("0,,,,.##T");
-        else if (length > 9)
+        else if (length > 12)
             return amount.ToString("0,,,.##B");
-        else if (length > 6) 
+        else if (length > 9) 
             return amount.ToString("0,,.##M");
-        else if (length > 3)
+        else if (length > 6)
             return amount.ToString("0,.##K");
         else
-            return amount.ToString("0");
+            return amount.ToString("0.##");
     }
 }
+
+/*
+      100.00
+    K 1,000.00 
+    M 1,000,000.00
+    B 1,000,000,000.00
+    T 1,000,000,000,000.00
+*/
